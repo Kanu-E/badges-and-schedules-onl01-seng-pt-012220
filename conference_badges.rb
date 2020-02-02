010e1d0162277a1c48e5fcfb7e0ad7468da69c4b
Chukwuemeka Kanu <chukwuemekakanu@gmail.com>
2020-02-02 19:58:39 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def badge_maker (name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator (names)
+  names.map { |name| badge_maker (name)}
+end
+
+def assign_rooms(speakers)
+  new_array = []
+  speakers.each_with_index do |name, i|
+  new_array << "Hello, #{name}! You'll be assigned to room #{i+1}!"
+  end
+ new_array
+end
+  
+  
+  
+  
+# def square_array(array)
+#   arr = []
+#   array.each { |i| arr << i ** 2 }
+#   return arr
+# end
